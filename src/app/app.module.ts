370264f0ee459ceee8917078b4e767f75a504b58
Nektarios Topalidis <nektariostopalides@gmail.com>
2023-12-09 18:54:01 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProcessFormModule } from './components/process-form/process-form.module';
+import { QueueComponent } from './components/queue/queue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    QueueComponent,
   ],
   imports: [
     BrowserModule,
