04e9412228e25947262f0fd37217fe668f3ee086
Nektarios Topalidis <nektariostopalides@gmail.com>
2023-12-24 23:08:43 +0200

    added quantum time field but without functionality

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,14 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrl: './chart.component.scss'
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+
+  quantumTimeForm!: FormGroup;
 
   @Output() start = new EventEmitter();
 
+  ngOnInit(): void {
+    this.quantumTimeForm = new FormGroup((
+      {
+        q: new FormControl()
+      }
+    ))
+  }
 
   sendStart(){
     this.start.emit();
--- src/app/components/chart/chart.module.ts
+++ src/app/components/chart/chart.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { ChartRoutingModule } from './chart-routing.module';
 import { ChartComponent } from './chart.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ChartComponent],
   imports: [
     CommonModule,
-    ChartRoutingModule
+    ChartRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     ChartComponent
